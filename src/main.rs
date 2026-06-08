a9d05d7a96e16d57b4e4d938102df4425a077f63
Louis Turpault <louis.turpault@gmail.com>
2026-06-08 12:23:44 -0400

    fix that

--- src/main.rs
+++ src/main.rs
@@ -25,7 +25,7 @@ fn main() {
     g.mount(&partitions[0], "/").unwrap();
 
     // read a file
-    let _data = g.read_file("/bin/cp").unwrap();
+    let _data = g.read_file("/usr/bin/cp").unwrap();
 
     // write a file
     // sync/umount/close
