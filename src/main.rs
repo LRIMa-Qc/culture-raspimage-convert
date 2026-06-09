2c06447c86da6986ce783e19fcd47fcf79bb4f11
Louis Turpault <louis.turpault@gmail.com>
2026-06-08 23:13:07 -0400

    added all files

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,4 @@
-use std::{
-    collections::HashMap,
-    fs,
-};
+use std::{collections::HashMap, fs};
 
 use guestfs::{AddDriveOptArgs, Handle};
 use minijinja::Environment;
@@ -29,14 +26,7 @@ fn main() {
     let partitions = g.list_partitions().unwrap();
     g.mount(&partitions[1], "/").unwrap();
 
-    handle_systemd_services(
-        String::from("/opt/LRIMa-central"),
-        String::from("/var/log/LRIMa/standard.log"),
-        String::from("/var/log/LRIMa/error.log"),
-        &g,
-    )
-    .unwrap();
-    handle_bluetooth_services(String::from("CONTROLLER_NAME"), &g).unwrap();
+    handle_all(&g).unwrap();
     // write a file
     // sync/umount/close
     g.sync().unwrap();
@@ -53,16 +43,23 @@ fn format_file_from_keys_in_template(
     tmpl.render(keys_in_template).unwrap()
 }
 fn handle_systemd_services(
-    working_directory: String,
-    standard_logs: String,
-    error_logs: String,
+    entries: &HashMap<String, String>,
     g: &Handle,
 ) -> Result<(), std::io::Error> {
     let current_file = fs::read_to_string("config_file/LRIMa-central.service")?;
     let mut missing_keys = HashMap::new();
-    missing_keys.insert(String::from("WORKING_DIRECTORY"), working_directory);
-    missing_keys.insert(String::from("STANDARD_LOGS"), standard_logs);
-    missing_keys.insert(String::from("ERROR_LOGS"), error_logs);
+    missing_keys.insert(
+        String::from("WORKING_DIRECTORY"),
+        entries.get("WORKING_DIRECTORY").unwrap().clone(),
+    );
+    missing_keys.insert(
+        String::from("STANDARD_LOGS"),
+        entries.get("STANDARD_LOGS").unwrap().clone(),
+    );
+    missing_keys.insert(
+        String::from("ERROR_LOGS"),
+        entries.get("ERROR_LOGS").unwrap().clone(),
+    );
 
     let formatted_file = format_file_from_keys_in_template(&current_file, missing_keys);
     let file_path = "/etc/systemd/system/LRIMa-central.service";
@@ -75,12 +72,89 @@ fn handle_systemd_services(
     Ok(())
 }
 
-fn handle_bluetooth_services(controller_name: String, g: &Handle) -> Result<(), std::io::Error> {
+fn handle_bluetooth_services(
+    entries: &HashMap<String, String>,
+    g: &Handle,
+) -> Result<(), std::io::Error> {
     let current_file = fs::read_to_string("config_file/bluetooth.conf")?;
     let mut missing_keys = HashMap::new();
-    missing_keys.insert(String::from("CONTROLLER_NAME"), controller_name);
+    missing_keys.insert(
+        String::from("CONTROLLER_NAME"),
+        entries.get("CONTROLLER_NAME").unwrap().clone(),
+    );
     let formatted_file = format_file_from_keys_in_template(&current_file, missing_keys);
     g.write("/etc/bluetooth/main.conf", formatted_file.as_bytes())
         .unwrap();
+    Ok(())
+}
+
+fn handle_config_file(
+    entries: &HashMap<String, String>,
+    g: &Handle,
+) -> Result<(), std::io::Error> {
+    let current_file = fs::read_to_string("config_file/config.ini")?;
+    let mut missing_keys = HashMap::new();
+    missing_keys.insert(
+        String::from("OBJ_ID"),
+        entries.get("OBJ_ID").unwrap().clone(),
+    );
+    missing_keys.insert(
+        String::from("AUTH_TOKEN"),
+        entries.get("AUTH_TOKEN").unwrap().clone(),
+    );
+
+    let formatted_file = format_file_from_keys_in_template(&current_file, missing_keys);
+    g.write("/opt/LRIMa-central/config.ini", formatted_file.as_bytes())
+        .unwrap();
+    Ok(())
+}
+
+fn handle_wifi_configuration(
+    entries: &HashMap<String, String>,
+    g: &Handle,
+) -> Result<(), std::io::Error> {
+    let current_file = fs::read_to_string("config_file/wpa_supplicant.conf")?;
+    let mut missing_keys = HashMap::new();
+    missing_keys.insert(
+        String::from("WIFI_SSID"),
+        entries.get("WIFI_SSID").unwrap().clone(),
+    );
+    missing_keys.insert(
+        String::from("WIFI_PRESHAREDKEY"),
+        entries.get("WIFI_PRESHAREDKEY").unwrap().clone(),
+    );
+
+    let formatted_file = format_file_from_keys_in_template(&current_file, missing_keys);
+    g.write("/etc/wpa_supplicant/wpa_supplicant.conf", formatted_file.as_bytes())
+        .unwrap();
+    Ok(())
+}
+fn handle_all(g: &Handle) -> Result<(), std::io::Error> {
+    let mut entries = HashMap::new();
+    entries.insert(
+        String::from("WORKING_DIRECTORY"),
+        String::from("/opt/LRIMa-central"),
+    );
+    entries.insert(
+        String::from("STANDARD_LOGS"),
+        String::from("/var/log/LRIMa/standard.log"),
+    );
+    entries.insert(
+        String::from("ERROR_LOGS"),
+        String::from("/var/log/LRIMa/error.log"),
+    );
+    entries.insert(String::from("CONTROLLER_NAME"), String::from("CONTROLLER_NAME"));
+    entries.insert(String::from("OBJ_ID"), String::from("OBJ_ID"));
+    entries.insert(String::from("AUTH_TOKEN"), String::from("AUTH_TOKEN"));
+    entries.insert(String::from("WIFI_SSID"), String::from("WIFI_SSID"));
+    entries.insert(
+        String::from("WIFI_PRESHAREDKEY"),
+        String::from("WIFI_PRESHAREDKEY"),
+    );
+
+    handle_systemd_services(&entries, g)?;
+    handle_bluetooth_services(&entries, g)?;
+    handle_config_file(&entries, g)?;
+    handle_wifi_configuration(&entries, g)?;
     Ok(())
 }
