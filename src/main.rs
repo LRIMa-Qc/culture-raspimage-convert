78616aebb8d25ff304d7f3e8879ae1f413d84e8f
Louis Turpault <louis.turpault@gmail.com>
2026-06-08 22:25:06 -0400

    fuckass

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,10 @@
 use std::{
     collections::HashMap,
-    error::Error,
-    fs::File,
-    io::{BufReader, Read},
-    str,
+    fs,
 };
 
 use guestfs::{AddDriveOptArgs, Handle};
-use minijinja::{Environment, context};
+use minijinja::Environment;
 fn main() {
     let g = Handle::create().unwrap();
     g.add_drive(
@@ -47,20 +44,13 @@ fn main() {
 }
 
 fn format_file_from_keys_in_template(
-    template: File,
+    template: &str,
     keys_in_template: HashMap<String, String>,
 ) -> String {
     let mut env = Environment::new();
-    let mut buf_read = BufReader::new(template);
-    let mut file_bytes = Vec::new();
-    buf_read.read_to_end(&mut file_bytes).unwrap();
-    let data_as_string = str::from_utf8(&file_bytes).unwrap();
-    env.add_template("file", data_as_string).unwrap();
+    env.add_template("file", template).unwrap();
     let tmpl = env.get_template("file").unwrap();
-    match tmpl.render(context! {keys_in_template}) {
-        Ok(val) => return val,
-        Err(e) => panic!("you done fucked up cowboy: {}", e),
-    }
+    tmpl.render(keys_in_template).unwrap()
 }
 fn handle_systemd_services(
     working_directory: String,
@@ -68,16 +58,14 @@ fn handle_systemd_services(
     error_logs: String,
     g: &Handle,
 ) -> Result<(), std::io::Error> {
-    let current_file = File::open("config_file/LRIMa-central.service").expect("fuck you mean");
-    println!("{:?}", current_file.metadata());
+    let current_file = fs::read_to_string("config_file/LRIMa-central.service")?;
     let mut missing_keys = HashMap::new();
     missing_keys.insert(String::from("WORKING_DIRECTORY"), working_directory);
     missing_keys.insert(String::from("STANDARD_LOGS"), standard_logs);
     missing_keys.insert(String::from("ERROR_LOGS"), error_logs);
 
-    let formatted_file = format_file_from_keys_in_template(current_file, missing_keys);
+    let formatted_file = format_file_from_keys_in_template(&current_file, missing_keys);
     let file_path = "/etc/systemd/system/LRIMa-central.service";
-    println!("{}", formatted_file.as_bytes().len());
     g.write(file_path, formatted_file.as_bytes()).unwrap();
     g.ln_s(
         file_path,
@@ -88,10 +76,10 @@ fn handle_systemd_services(
 }
 
 fn handle_bluetooth_services(controller_name: String, g: &Handle) -> Result<(), std::io::Error> {
-    let current_file = File::open("config_file/bluetooth.conf")?;
+    let current_file = fs::read_to_string("config_file/bluetooth.conf")?;
     let mut missing_keys = HashMap::new();
     missing_keys.insert(String::from("CONTROLLER_NAME"), controller_name);
-    let formatted_file = format_file_from_keys_in_template(current_file, missing_keys);
+    let formatted_file = format_file_from_keys_in_template(&current_file, missing_keys);
     g.write("/etc/bluetooth/main.conf", formatted_file.as_bytes())
         .unwrap();
     Ok(())
