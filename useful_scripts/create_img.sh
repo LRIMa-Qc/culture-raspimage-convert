ac73dc10ecee2f697ebd89a7026b308f93f63aa6
Louis Turpault <louis.turpault@gmail.com>
2026-06-15 17:18:18 -0400

    stuff

--- useful_scripts/create_img.sh
+++ useful_scripts/create_img.sh
@@ -7,3 +7,4 @@ rm -f 2026-04-21-raspios-trixie-arm64-lite.img
 cp 2026-04-21-raspios-trixie-arm64-lite.img.xz.bck 2026-04-21-raspios-trixie-arm64-lite.img.xz
 xz -d 2026-04-21-raspios-trixie-arm64-lite.img.xz;
 cargo run --release -- --raspberry-pi-image-file-path 2026-04-21-raspios-trixie-arm64-lite.img --config-file-path config.json
+echo "we did it "
